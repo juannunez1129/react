a0ece017147c0944d9a5058b65feca071670d44b
Juan Nunez <Juan.Nunez612@gmail.com>
2021-02-12 12:52:22 -0600

    Addded LI to list the todos

--- todo/src/Components/Todo.jsx
+++ todo/src/Components/Todo.jsx
@@ -1,6 +1,18 @@
-import react from 'react'
-
+import react, { useContext } from 'react'
+import TodosContext from '../context'
 
 export default function Todo(){
-    
+const { state } = useContext(TodosContext)
+
+return (
+    <div>
+        <ul>
+            {state.todos.maps(todo => (
+                <li key = {todo.id}>
+                    <span>{todo.text}</span>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
 }
