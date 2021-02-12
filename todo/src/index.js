96fcf2942e6c5c27bd31b12d342fc19d8ec408ab
Juan Nunez <Juan.Nunez612@gmail.com>
2021-02-12 12:13:04 -0600

    added reducer and context files and deleted the app.js file

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useContext, useReducer} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  
+}
 
 ReactDOM.render(
   <React.StrictMode>
