30dd183366820edc21c8c794b2eca06c616af71f
Juan Nunez <Juan.Nunez612@gmail.com>
2021-02-12 14:45:53 -0600

    added a toggle to strike through for double click of a todo

--- todo/src/Components/Todo.jsx
+++ todo/src/Components/Todo.jsx
@@ -2,7 +2,7 @@ import react, { useContext } from 'react'
 import TodosContext from '../context'
 
 export default function Todo(){
-const { state } = useContext(TodosContext);
+const { state,dispatch } = useContext(TodosContext);
 const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing to do today!"
 
 return (
@@ -11,7 +11,9 @@ return (
         <ul className = "list-reset text-black p-0">
             {state.todos.map(todo => (
                 <li key = {todo.id} className = "flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4">
-                    <span className ="flex-1 m1-12 cursor-pointer">{todo.text}</span>
+                    <span 
+                    onDoubleClick={() => dispatch({ type: "TOGGLE_TODO", payload: todo })} 
+                    className ={`flex-1 m1-12 cursor-pointer ${todo.complete && "line-through text-grey-darkest"}`}>{todo.text}</span>
                 <button>
                 <img src="https://img.icons8.com/android/24/000000/edit.png" alt= "Edit icon" className = "h-6"/>
                 </button>
